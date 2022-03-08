ed7b714ac5d9e8f25f57f2c152d7038d73961cd6
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-03-09 01:54:14 +0900

    modified

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"go-chat/handler"
+	"net/http"
 
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.POST("/message", handler.PostMessageHandler)
+	e.Logger.Fatal(e.Start(":1323"))
 }
